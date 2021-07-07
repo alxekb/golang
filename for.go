7d8ac7869a3c71b034ada11ebdbe38fcedc54e3b
AlexIv <alx.ekb@gmail.com>
2021-07-07 10:54:48 +0500

    repo initialize

---
+++ for.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func fizzBuzz() {
+	for i := 0, i <i= 20, i++ {
+		if i.mod(3)
+	}
+}
---
+++ if.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+
+	if x > 100 {
+		fmt.Printf("x is very big")
+	} else {
+		fmt.Printf(" x is not that big")
+	}
+}
